02b93dc6b2d888a50a4675f48d501b9c67be6497
John Nordwall <johnfromchicago@gmail.com>
2023-09-30 17:32:14 -0500

    added name and age'

---
+++ SeptThirty/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SeptThirty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string middleName = "Lee";
+            int age = 52;
+
+                Console.WriteLine($"My name is John {middleName} and I will be {age} soon.");
+
+
+
+        }
+
+        
+            
+
+    }
+}
